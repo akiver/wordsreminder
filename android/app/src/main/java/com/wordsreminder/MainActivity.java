c46872aaa62d08f02c3f166f4c254e623a3674b7
AkiVer <ys.renaud@gmail.com>
2019-09-29 13:40:54 -0700

    chore: upgrade rn 0.61

--- android/app/src/main/java/com/wordsreminder/MainActivity.java
+++ android/app/src/main/java/com/wordsreminder/MainActivity.java
@@ -11,9 +11,9 @@ import com.zoontek.rnbootsplash.RNBootSplash;
 public class MainActivity extends ReactActivity {
 
     /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
     @Override
     protected String getMainComponentName() {
         return "WordsReminder";
