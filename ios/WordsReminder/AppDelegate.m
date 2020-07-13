c05b71878bf9c574507d0439d020f9d46d7b2d82
AkiVer <ys.renaud@gmail.com>
2020-07-13 10:07:07 -0700

    chore: upgrade deps

--- ios/WordsReminder/AppDelegate.m
+++ ios/WordsReminder/AppDelegate.m
@@ -6,7 +6,7 @@
 #import <React/RCTRootView.h>
 #import "RNBootSplash.h"
 
-#if DEBUG
+#ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
 #import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
@@ -28,7 +28,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  #if DEBUG
+  #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
   #endif
 
